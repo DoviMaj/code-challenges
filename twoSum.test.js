e54513e2954b857f7684ff7226d865738a30881d
dovimaj <dovimajowka@gmail.com>
2021-07-06 13:33:50 +0300

    twoSum solution and tests

---
+++ twoSum.test.js
@@ -0,0 +1,11 @@
+const twoSum = require("./twoSum.js");
+
+test("Case 1", () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+});
+test("Case 2", () => {
+  expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
+});
+test("Case 3", () => {
+  expect(twoSum([3, 3], 6)).toEqual([0, 1]);
+});
