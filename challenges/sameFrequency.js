626aba6fdb1f2fb2bacb259bd246af6e4241ae83
dovimaj <dovimajowka@gmail.com>
2021-07-07 20:02:43 +0300

    sameFrequency solution

---
+++ challenges/sameFrequency.js
@@ -0,0 +1,15 @@
+// Given two positive integers, find out if two numbers have the same frequency of digits
+
+function sameFrequency(a, b) {
+  let lookup = {};
+  for (let char of a.toString()) {
+    lookup[char] ? lookup[char]++ : (lookup[char] = 1);
+  }
+  for (let char of b.toString()) {
+    if (!lookup[char]) return false;
+    lookup[char]--;
+  }
+  return true;
+}
+
+console.log(sameFrequency(22, 222));
