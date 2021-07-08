f51241d87c169a99d2849887cf1b247fc1abe0ee
dovimaj <dovimajowka@gmail.com>
2021-07-08 16:36:49 +0300

    Remove unsolved solutions

--- challenges/maxFamilySeats.js
+++
@@ -1,6 +0,0 @@
-// maximum amount of 4 people siting next to each other or pairs of twos separated by asile that are not included in S which is a string of seats as pairs like: '1A 2F', N is the amount of rows, N starts at 1
-function maxFamilySeats(N, S) {
-  // to solve
-}
-
-// console.log(maxFamilySeats(2, '1A 2F 1C'));
