8ede6e14fed3ed9450fb95a35ecb7c5d68ac119f
dovimaj <dovimajowka@gmail.com>
2021-07-08 19:43:23 +0300

    Add isSubsequence solution

---
+++ challenges/isSubsequence.js
@@ -0,0 +1,15 @@
+// Check if string a appear inside string b in order
+function isSubsequence(a, b) {
+  let i = 0;
+  for (let j = 0; j <= b.length; j++) {
+    if (i === a.length) return true;
+    if (a[i] === b[j]) {
+      i++;
+    }
+  }
+  return false;
+}
+
+console.log(isSubsequence("hello", "hello world")); // true
+console.log(isSubsequence("sing", "sting")); // true
+console.log(isSubsequence("abc", "acb")); // false
